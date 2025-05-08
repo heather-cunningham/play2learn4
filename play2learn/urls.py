ca193e5859c7a603a7b8d40cdfdc96222bb78976
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-08 14:58:52 -0400

    start-pages-app: Home and About views and URLs configs

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin App 
     path("admin/", admin.site.urls),
-    path('', include("games.urls"))
+
+    # Local Apps
+    path("", include("games.urls")),
+    path("", include("pages.urls")),
 ]
