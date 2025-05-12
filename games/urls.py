8f099023c282106b9c3b57d63d979b7511d9a2ab
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-12 17:40:02 -0400

    start-over: added links for the games in the main nav

--- games/urls.py
+++ games/urls.py
@@ -6,6 +6,6 @@ app_name = 'games'
 
 
 urlpatterns = [
-    path('math-facts/', MathFactsView.as_view(), name='math-facts'),
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
+    path('math-facts/', MathFactsView.as_view(), name='math-facts'),
 ]
