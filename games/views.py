671145f9e214d6f05c2518f4e9a33b906419e250
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-12 16:58:28 -0400

    start-over: put games > views in alphabetical order

--- games/views.py
+++ games/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class MathFactsView(TemplateView):
-    template_name = "games/math-facts.html"
-
-
 class AnagramHuntView(TemplateView):
     template_name = "games/anagram-hunt.html"
+
+
+class MathFactsView(TemplateView):
+    template_name = "games/math-facts.html"
