65e666ea30ed53969e09e6a776654828c8c60c01
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-13 12:46:42 -0400

    start-models: games > models > GameScore model & ran migrations

--- games/models.py
+++ games/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class GameScore(models.Model):
+    ANAGRAM_GAME = "ANAGRAM_GAME"
+    MATH_GAME = "MATH_GAME"
+
+    GAME_CHOICES = [
+        (ANAGRAM_GAME, "Anagram Hunt"),
+        (MATH_GAME, "Math Facts"),
+    ]
+
+
+    username = models.TextField()
+    game = models.TextField(choices=GAME_CHOICES, default=ANAGRAM_GAME)
+    score = models.IntegerField()
+    game_datetime = models.DateTimeField(auto_now_add=True)
+ 
