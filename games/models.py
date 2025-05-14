c6104b9f2b74c7c2e0ee4e9aa5a6c46b0c690f2c
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-14 16:30:35 -0400

    cont-models: My FinalScore model, done my way

--- games/models.py
+++ games/models.py
@@ -1,17 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class GameScore(models.Model):
-    ANAGRAM = "ANAGRAM"
-    MATH = "MATH"
+class FinalScore(models.Model):
+    ANAGRAM = "anagram_hunt"
+    MATH = "math_facts"
     #
-    GAME_CHOICES = [
+    GAME_NAME_CHOICES = [
         (ANAGRAM, "Anagram Hunt"),
         (MATH, "Math Facts"),
     ]
     #
-    user_name = models.TextField()
-    game = models.TextField(choices=GAME_CHOICES, default=MATH)
-    score = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    game_name = models.CharField(max_length=25, choices=GAME_NAME_CHOICES, default=ANAGRAM)
+    ## Unstructured JSON field to hold any game-specific data as JSON, e.g.:
+    #  settings={
+    #     "operation": "addition",
+    #     "max_number": 10
+    # }    
+    settings = models.JSONField()
+    final_score = models.IntegerField()
+    game_date_time = models.DateTimeField(auto_now_add=True)
  
