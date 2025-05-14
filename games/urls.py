8c75d54c277e280e516eb836ae323e1a23a02cc7
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-14 14:17:16 -0400

    start-models: My bug in game-scores, fixed.  You set the path in the urls.py to the wrong view.

--- games/urls.py
+++ games/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from games.views import MathFactsView, AnagramHuntView, record_score
+from games.views import AnagramHuntView, GameScoresView, MathFactsView, record_score
 
 
 app_name = 'games'
@@ -9,5 +9,5 @@ urlpatterns = [
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
     path("record-score/", record_score, name="record-score"),
-    path("game-scores/", record_score, name="game-scores"),
+    path("game-scores/", GameScoresView.as_view(), name="game-scores"),
 ]
