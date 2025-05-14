c7f90d411839b81b80ef790ee868753df8cac8a6
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-14 12:41:44 -0400

    start-db: Postgresql Db settings.py in main app

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -82,11 +82,23 @@ WSGI_APPLICATION = "play2learn.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+## Default SQL db which comes with Django
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'play2learn4',
+        'USER': 'postgres',
+        'PASSWORD': '3l3ph0ntsNeverForget',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
