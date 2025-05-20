cfa08afb8b668059ae063f786f40b3d0cf1a394c
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-20 10:52:06 -0400

    user-models-n-db: Installed psycopg2 db adapter and added Postgresql db in main app Settings

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -84,10 +84,18 @@ WSGI_APPLICATION = "play2learn.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': '3l3ph0ntsNeverForget',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
