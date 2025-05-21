fa154276035f828e0e73b2a0489fa3a14c47b02c
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-21 13:41:34 -0400

    setup-emailing-n-forms: ReviewForm: added two default widget changes

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    first_name = forms.CharField()
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    comments = forms.CharField()
+    comments = forms.CharField(
+        widget=forms.Textarea()
+    )
