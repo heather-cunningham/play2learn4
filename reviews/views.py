3e6c945901ca5472a8d085ce7db0505b73c39a4f
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-21 12:09:33 -0400

    setup-emailing-n-forms:  Added the needed 2 simple Review views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+from reviews.forms import ReviewForm
 
-# Create your views here.
+
+class ReviewFormView(FormView):
+    template_name = "reviews/review-form.html"
+    form_class = ReviewForm
+    success_url = reverse_lazy("reviews:thanks")
+
+
+class ReviewThanksView(TemplateView):
+    template_name = "reviews/thanks.html"
