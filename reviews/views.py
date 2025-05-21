c36209d242366382e1d2afc0af6030435e43b5d3
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-21 13:26:05 -0400

    setup-emailing-n-forms: ReviewFormView: form_valid() fcn wch also sends an email to PR Mngr

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,31 @@
+import html
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from common.utils.email import send_email
 from reviews.forms import ReviewForm
 
-
+## BEGIN class
 class ReviewFormView(FormView):
     template_name = "reviews/review-form.html"
     form_class = ReviewForm
     success_url = reverse_lazy("reviews:thanks")
+
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        to = "cunningham.heatherirene@gmail.com"
+        subject = "Review submitted"
+        content = f'''<p>Hey PR Manager!</p>
+            <p>Play2Learn feedback received:</p>
+            <ol>'''
+        for key, value in data.items():
+            label = key.replace('_', ' ').title()
+            entry = html.escape(str(value), quote=False)
+            content += f'<li>{label}: {entry}</li>' 
+        content += '</ol>'
+        send_email(to, subject, content)
+        return super().form_valid(form)
+## END class ReviewFormView
 
 
 class ReviewThanksView(TemplateView):
