f71f47c05d586c7f35348f43481593e536b9e09d
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-23 13:34:10 -0400

    setup-emailing-n-forms: Reg ContactUs in Django Admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from pages.models import ContactUs
 
-# Register your models here.
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    model = ContactUs
+    list_display = ["first_name", "last_name", "email", "username",  "user", "comments",
+                     "contact_date_time"]
+    #
+    #
+    def get_readonly_fields(self, request, obj=None):
+        if(obj):
+            return ("first_name", "last_name", "comments", "contact_date_time")
+        return ()
