49556b45a39bd9c3d29d97e935e7558c170de6e0
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-27 11:13:46 -0400

    user-reg-n-auth: Pip installed Pillow to use images. Filled out CustomUser model & ran migrations

--- users/models.py
+++ users/models.py
@@ -1,5 +1,28 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
 from django.db import models
+from django.urls import reverse
+
+
+def validate_avatar(img):
+    width, height = get_image_dimensions(img)
+    if(width > 200 or height > 200):
+        raise ValidationError("Avatar/profile pic must be no larger than 200 x 200 pixels.")
+    return  ## Former college prof convention to mark the end of the methods in Python 
+
 
 class CustomUser(AbstractUser):
-    pass
+    joined = models.DateTimeField(verbose_name="Date Joined", auto_now_add=True, blank=True, null=True)
+    birthdate = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, 
+                               help_text="Must be a jpg, jpeg, jfif, or png no larger than 200 x 200px.", 
+                               validators=[validate_avatar])
+    #
+    #
+    def get_absolute_url(self):
+        return reverse('my-account')
+    #
+    #
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, Username: {self.username}, Date Joined: {self.joined}"
