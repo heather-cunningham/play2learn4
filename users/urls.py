bf718b76c2e95e3b9a182a097487f7ce9b3332eb
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-27 12:13:19 -0400

    user-reg-n-auth: MyAccount > Change Password: view, form, & urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import MyAccountView
+from users.views import CustomPasswordChangeView, MyAccountView
 
 
 urlpatterns = [
+    path("password/change/", CustomPasswordChangeView.as_view(),name="account_change_password"),
     path("my-account/", MyAccountView.as_view(), name="my-account"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 from django.views.generic import UpdateView
+from allauth.account.views import PasswordChangeView
 from users.forms import CustomUserChangeForm
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy("my-account")
 
 
 class MyAccountView(UpdateView):
