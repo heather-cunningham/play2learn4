1066de0a529a8f8ad8c66210dd9372bc199464f3
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-31 11:18:06 -0400

    media: Settings and urls

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     ## Admin app
@@ -13,4 +16,4 @@ urlpatterns = [
     path("", include("games.urls")),
     path("", include("pages.urls")),
     path("", include("reviews.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
