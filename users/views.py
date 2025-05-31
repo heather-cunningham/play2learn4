609b5c89837c4339860331e4f4d6377cb099ca67
heather-irene <cunningham.heatherirene@gmail.com>
2025-05-31 15:45:29 -0400

    msgs: Success and Error alerts/msgs for MyAccount

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from allauth.account.views import PasswordChangeView
@@ -10,9 +11,11 @@ class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy("my-account")
 
 
-class MyAccountView(LoginRequiredMixin, UpdateView):
+class MyAccountView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
+    success_message = "Account/profile updated successfully!"
+    error_message = "Error updating account/profile."
     template_name = "account/my_account.html"
     #
     #
