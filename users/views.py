cdb0b86e92ffc7537505a16e3082c54668befbb7
heather-irene <cunningham.heatherirene@gmail.com>
2025-06-02 10:33:49 -0400

    msgs: Proper implementation of Error alerts/msgs for MyAccount

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -15,10 +16,15 @@ class MyAccountView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
     success_message = "Account/profile updated successfully!"
-    error_message = "Error updating account/profile."
     template_name = "account/my_account.html"
     #
     #
     ## @override
     def get_object(self):
         return self.request.user
+    #
+    # @override
+    def form_invalid(self, form):
+        error_message = "Error updating account/profile."
+        messages.error(self.request, error_message)
+        return super().form_invalid(form)
